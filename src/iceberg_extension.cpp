6a23f8a915d9fd09d89fad202208f7ea16505c16
Sam Ansmink <samansmink@hotmail.com>
2023-02-23 14:06:06 +0100

    basic ICEBERG SNAPSHOTS function

--- src/iceberg_extension.cpp
+++ src/iceberg_extension.cpp
@@ -1,6 +1,8 @@
 #define DUCKDB_EXTENSION_MAIN
 
 #include "iceberg_extension.hpp"
+#include "iceberg_functions.hpp"
+
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/common/string_util.hpp"
@@ -12,46 +14,19 @@
 namespace duckdb {
 
 // TODO 2:
-// Table Function ICEBERG_SNAPSHOTS('path')
-// - Read the version hint
-// - Read the Json thing
-// - List snapshots with all the info we want
-
-// TODO 2:
 // - hello world with avro
 
-inline void JsonHelloWorld(DataChunk &args, ExpressionState &state, Vector &result) {
-    auto &name_vector = args.data[0];
-    UnaryExecutor::Execute<string_t, string_t>(
-        name_vector, result, args.size(),
-        [&](string_t json_string) {
-
-          // Read JSON and get root
-          yyjson_doc *doc = yyjson_read(json_string.GetDataUnsafe(), json_string.GetSize(), 0);
-          yyjson_val *root = yyjson_doc_get_root(doc);
-
-          // Get root["hello"]
-          yyjson_val *hello_value = yyjson_obj_get(root, "hello");
-
-          string_t res = StringVector::AddString(result, yyjson_get_str(hello_value), yyjson_get_len(hello_value));
-
-          // Free the doc
-          yyjson_doc_free(doc);
-
-          return res;
-        });
-}
-
 static void LoadInternal(DatabaseInstance &instance) {
-	Connection con(instance);
+    Connection con(instance);
     con.BeginTransaction();
+    auto &context = *con.context;
 
     auto &catalog = Catalog::GetSystemCatalog(*con.context);
 
-    CreateScalarFunctionInfo iceberg_fun_info(
-            ScalarFunction("hello_world_yyjson", {LogicalType::VARCHAR}, LogicalType::VARCHAR, JsonHelloWorld));
-    iceberg_fun_info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
-    catalog.CreateFunction(*con.context, &iceberg_fun_info);
+    // Iceberg Table Functions
+    for (auto &fun : IcebergFunctions::GetTableFunctions()) {
+      catalog.CreateTableFunction(context, &fun);
+    }
 
     con.Commit();
 }
--- src/iceberg_functions/iceberg_snapshots.cpp
+++ src/iceberg_functions/iceberg_snapshots.cpp
@@ -8,26 +8,44 @@
 
 namespace duckdb {
 
-struct IcebergSnapshotsInfo : public TableFunctionInfo {
-public:
-  explicit IcebergSnapshotsInfo() = default;
-};
-
 struct IcebergSnapshotGlobalTableFunctionState : public GlobalTableFunctionState {
 public:
     static unique_ptr<GlobalTableFunctionState> Init(ClientContext &context,TableFunctionInitInput &input) {
       return make_unique<GlobalTableFunctionState>();
     }
-    idx_t MaxThreads(){ return 1; };
 };
 
 struct IcebergSnaphotsBindData: public TableFunctionData {
-    idx_t table_version;
+    IcebergSnaphotsBindData() : table_version(0), metadata_doc(nullptr){};
+    ~IcebergSnaphotsBindData() {
+        if (metadata_doc) {
+            yyjson_doc_free(metadata_doc);
+        }
+    }
 
+    idx_t table_version;
     string metadata_file;
     yyjson_doc *metadata_doc;
-    yyjson_val *metadata_snapshots;
+    yyjson_arr_iter snapshot_it;
 };
+
+struct IceBergSnapshot {
+    idx_t snapshot_id;
+    idx_t sequence_number;
+    idx_t schema_id;
+    string manifest_list;
+    idx_t timestamp_ms;
+};
+
+static IceBergSnapshot ParseSnapShot(yyjson_val* val) {
+    IceBergSnapshot ret;
+    ret.snapshot_id = yyjson_get_uint(yyjson_obj_get(val, "snapshot-id"));
+    ret.sequence_number = yyjson_get_uint(yyjson_obj_get(val, "sequence-number"));
+    ret.schema_id = yyjson_get_uint(yyjson_obj_get(val, "schema-id"));
+    ret.timestamp_ms = yyjson_get_uint(yyjson_obj_get(val, "timestamp-ms"));
+    ret.manifest_list = yyjson_get_str(yyjson_obj_get(val, "manifest-list"));
+    return ret;
+}
 
 static string file_to_string(FileSystem& fs, const string& path) {
     auto handle = fs.OpenFile(path, FileFlags::FILE_FLAGS_READ);
@@ -45,8 +63,10 @@ static unique_ptr<FunctionData> IcebergSnapshotsBind(ClientContext &context, Tab
     auto filename = input.inputs[0].ToString();
     FileSystem &fs = FileSystem::GetFileSystem(context);
 
+    auto meta_path = FileSystem::JoinPath(filename, "metadata");
+
     // First read the version hint
-    auto version_file_path = FileSystem::JoinPath(filename, "version-hint.text");
+    auto version_file_path = FileSystem::JoinPath(meta_path, "version-hint.text");
     auto version_file_content = file_to_string(fs, version_file_path);
 
     try {
@@ -58,17 +78,16 @@ static unique_ptr<FunctionData> IcebergSnapshotsBind(ClientContext &context, Tab
     }
 
     // Then read the metadata json file
-    auto metadata_file_path =  FileSystem::JoinPath(filename, "v" + to_string(ret->table_version) + ".metadata.json");
+    auto metadata_file_path =  FileSystem::JoinPath(meta_path, "v" + to_string(ret->table_version) + ".metadata.json");
     ret->metadata_file = file_to_string(fs, metadata_file_path);
-
 
     // Ensure we can read the snapshots property here
     ret->metadata_doc = yyjson_read(ret->metadata_file.c_str(), ret->metadata_file.size(), 0);
-    auto root = yyjson_doc_get_root(ret->metadata_doc);
-    ret->metadata_snapshots = yyjson_obj_get(root, "snapshots");
 
-    // Free the doc
-//    yyjson_doc_free(doc);
+    auto root = yyjson_doc_get_root(ret->metadata_doc);
+    auto snapshots = yyjson_obj_get(root, "snapshots");
+
+    yyjson_arr_iter_init(snapshots, &ret->snapshot_it);
 
     names.emplace_back("sequence_number");
     return_types.emplace_back(LogicalType::BIGINT);
@@ -79,34 +98,43 @@ static unique_ptr<FunctionData> IcebergSnapshotsBind(ClientContext &context, Tab
     names.emplace_back("timestamp-ms");
     return_types.emplace_back(LogicalType::BIGINT);
 
+    names.emplace_back("schema-id");
+    return_types.emplace_back(LogicalType::BIGINT);
+
     names.emplace_back("manifest-list");
     return_types.emplace_back(LogicalType::VARCHAR);
 
-    names.emplace_back("schema-id");
-    return_types.emplace_back(LogicalType::VARCHAR);
-
-    return make_unique<TableFunctionData>();
+    return ret;
 }
 
 // Snapshots function
 static void IcebergSnapshotsFunction(ClientContext &context, TableFunctionInput &data, DataChunk &output) {
-    auto& glob_state = (IcebergSnapshotGlobalTableFunctionState&)*data.global_state;
+    auto bind_data = (IcebergSnaphotsBindData*)data.bind_data;
 
-    if (glob_state.csv_file.empty()) {
-        auto& opener = *FileOpener::Get(context);
-        FileSystem &fs = FileSystem::GetFileSystem(context);
-        auto handle = fs.OpenFile(file_name, FileFlags::FILE_FLAGS_READ, FileSystem::DEFAULT_LOCK,
-                                  FileSystem::DEFAULT_COMPRESSION, FileSystem::GetFileOpener(context));
+    idx_t i = 0;
+    while (auto next_snapshot = yyjson_arr_iter_next(&bind_data->snapshot_it)) {
+        if (i >= STANDARD_VECTOR_SIZE) {
+            break;
+        }
+
+        auto snapshot = ParseSnapShot(next_snapshot);
+
+        FlatVector::GetData<uint64_t>(output.data[0])[i] = snapshot.sequence_number;
+        FlatVector::GetData<uint64_t>(output.data[1])[i] = snapshot.snapshot_id;
+        FlatVector::GetData<uint64_t>(output.data[2])[i] = snapshot.timestamp_ms;
+        FlatVector::GetData<uint64_t>(output.data[3])[i] = snapshot.schema_id;
+
+        string_t manifest_string_t = StringVector::AddString(output.data[4],string_t(snapshot.manifest_list));
+        FlatVector::GetData<string_t>(output.data[4])[i] = manifest_string_t;
+
+        i++;
     }
+    output.SetCardinality(i);
 }
 
-
-
 CreateTableFunctionInfo IcebergFunctions::GetIcebergSnapshotsFunction() {
-    TableFunctionSet function_set("iceberg_snapshots");
-    auto function_info = make_shared<IcebergSnapshotsInfo>();
-
-    TableFunction table_function({LogicalType::VARCHAR}, IcebergSnapshotsFunction, IcebergSnapshotsBind, IcebergSnapshotGlobalTableFunctionState::Init);
+    auto function_info = make_shared<TableFunctionInfo>();
+    TableFunction table_function("iceberg_snapshots", {LogicalType::VARCHAR}, IcebergSnapshotsFunction, IcebergSnapshotsBind, IcebergSnapshotGlobalTableFunctionState::Init);
     table_function.projection_pushdown = false;
     table_function.function_info = std::move(function_info);
 
